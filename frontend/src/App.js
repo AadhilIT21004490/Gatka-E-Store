61f449e6fb46fb0171a77fe7d868a7c1a2dfd522
it21004490 <it21004490@my.sliit.lk>
2023-02-27 13:42:26 +0530

    Bootstrap setup,Header and footer components setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <h1>Welcome to Gatka E-shop</h1>
-    </>
-  );
+  <>
+    <Header/>  
+    <main className='py-3'>
+      <Container>
+
+        <h1>Welcome to Gatka E-shop </h1>
+        <h2>Sakkikliyan haalll...Sajaji..Inthikhaf..Aashif!!! </h2>
+
+      </Container>
+      
+    </main>
+    <Footer/>
+  </>
+  ); 
 }
 
 export default App;
