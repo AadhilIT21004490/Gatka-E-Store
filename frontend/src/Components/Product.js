85bcbff7ba1f293c2a0160517f5b611ea4abfa74
it21004490 <it21004490@my.sliit.lk>
2023-03-10 13:29:42 +0530

    Rating icon implementation done

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
@@ -14,10 +15,12 @@ const Product = ({product}) => {
             </Card.Title>
         </a>
 
-        <Card.Text>
-            <div className='my-3'>
-                {product.rating} from {product.numReviews} reviews
-            </div>
+        <Card.Text as='div'>
+            <Rating 
+            value={product.rating} 
+            text={` ${product.numReviews} reviews`} 
+            color='gold'
+            />
         </Card.Text>
         <Card.Text as='h3'>${product.price}</Card.Text>
 
